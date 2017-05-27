fe6dd1e0db9e942989c11269a71a011e2151a5f7
wangxi <wangxiaoxinyist@gmail.com>
2017-05-27 23:23:22 +0800

    service and promise

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,7 +17,7 @@ const HEROES: Hero[] = [
 
 @Injectable()
 export class HeroService {
-    getHeroes(): Hero[] {
-        return HEROES;
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
     }
 }
